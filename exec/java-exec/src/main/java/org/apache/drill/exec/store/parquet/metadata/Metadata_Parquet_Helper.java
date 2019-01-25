80e17e1d8932e07a2c2d67c49fa7346b684635a2
Venkata Jyothsna Donapati <jyothsna.dvj@gmail.com>
2019-01-25 11:03:50 -0800

    Removed writing column metadata

--- exec/java-exec/src/main/java/org/apache/drill/exec/store/parquet/metadata/Metadata_Parquet_Helper.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/store/parquet/metadata/Metadata_Parquet_Helper.java
@@ -56,44 +56,44 @@ public class Metadata_Parquet_Helper {
       schema.append("required INT64 rowCount; \n");
       schema.append("required binary hostAffinity; \n");
 
-      for (MetadataBase.ColumnMetadata column : parquetTableMetadata.getFiles().get(0).getRowGroups().get(0).getColumns()) {
-        String[] columnName = column.getName();
-        String name = String.join(":", column.getName());
-        schema.append("required binary " + name + "_name; \n");
-//        schema.append("required binary " + name + "_columnStats; \n");
-
-       /* TypeProtos.MajorType type = ParquetReaderUtility.getType(parquetTableMetadata.getPrimitiveType(columnName), parquetTableMetadata.getOriginalType(columnName), 0, 0);
-        switch (type.getMinorType()) {
-          case INT:
-          case TIME:
-            schema.append("required INT32 " + name + "_minValue; \n");
-            schema.append("required INT32 " + name + "_maxValue; \n");
-            break;
-          case BIGINT:
-          case TIMESTAMP:
-            schema.append("required INT64 " + name + "_minValue; \n");
-            schema.append("required INT64 " + name + "_maxValue; \n");
-            break;
-          case FLOAT4:
-            schema.append("required FLOAT " + name + "_minValue; \n");
-            schema.append("required FLOAT " + name + "_maxValue; \n");
-            break;
-          case FLOAT8:
-            schema.append("required DOUBLE " + name + "_minValue; \n");
-            schema.append("required DOUBLE " + name + "_maxValue; \n");
-            break;
-          case BIT:
-            schema.append("required BOOLEAN " + name + "_minValue; \n");
-            schema.append("required BOOLEAN " + name + "_maxValue; \n");
-            break;
-          default:
-            schema.append("required binary " + name + "_minValue; \n");
-            schema.append("required binary " + name + "_maxValue; \n");
-        }*/
-        schema.append("required binary " + name + "_minValue; \n");
-        schema.append("required binary " + name + "_maxValue; \n");
-        schema.append("required INT64 " + name + "_nulls; \n");
-      }
+//      for (MetadataBase.ColumnMetadata column : parquetTableMetadata.getFiles().get(0).getRowGroups().get(0).getColumns()) {
+//        String[] columnName = column.getName();
+//        String name = String.join(":", column.getName());
+//        schema.append("required binary " + name + "_name; \n");
+////        schema.append("required binary " + name + "_columnStats; \n");
+//
+//       /* TypeProtos.MajorType type = ParquetReaderUtility.getType(parquetTableMetadata.getPrimitiveType(columnName), parquetTableMetadata.getOriginalType(columnName), 0, 0);
+//        switch (type.getMinorType()) {
+//          case INT:
+//          case TIME:
+//            schema.append("required INT32 " + name + "_minValue; \n");
+//            schema.append("required INT32 " + name + "_maxValue; \n");
+//            break;
+//          case BIGINT:
+//          case TIMESTAMP:
+//            schema.append("required INT64 " + name + "_minValue; \n");
+//            schema.append("required INT64 " + name + "_maxValue; \n");
+//            break;
+//          case FLOAT4:
+//            schema.append("required FLOAT " + name + "_minValue; \n");
+//            schema.append("required FLOAT " + name + "_maxValue; \n");
+//            break;
+//          case FLOAT8:
+//            schema.append("required DOUBLE " + name + "_minValue; \n");
+//            schema.append("required DOUBLE " + name + "_maxValue; \n");
+//            break;
+//          case BIT:
+//            schema.append("required BOOLEAN " + name + "_minValue; \n");
+//            schema.append("required BOOLEAN " + name + "_maxValue; \n");
+//            break;
+//          default:
+//            schema.append("required binary " + name + "_minValue; \n");
+//            schema.append("required binary " + name + "_maxValue; \n");
+//        }*/
+//        schema.append("required binary " + name + "_minValue; \n");
+//        schema.append("required binary " + name + "_maxValue; \n");
+//        schema.append("required INT64 " + name + "_nulls; \n");
+//      }
     }
     schema.append("} \n");
     return schema.toString();
@@ -121,44 +121,44 @@ public class Metadata_Parquet_Helper {
                 .append("rgLength", rowGroup.getLength())
                 .append("rowCount", rowGroup.getRowCount())
                 .append("hostAffinity", gson.toJson(rowGroup.getHostAffinity()));
-                for (MetadataBase.ColumnMetadata column : rowGroup.getColumns()) {
-                  String[] columnName = column.getName();
-                  String name = String.join(":", columnName);
-                  simpleGroup.append(name + "_name", name);
-//                  simpleGroup.append(name + "_columnStats", gson.toJson(column));
-                  simpleGroup.append(name + "_minValue", String.valueOf(column.getMinValue()));
-                  simpleGroup.append(name + "_maxValue", String.valueOf(column.getMaxValue()));
-               /*   TypeProtos.MajorType type = ParquetReaderUtility.getType(parquetTableMetadata.getPrimitiveType(columnName), parquetTableMetadata.getOriginalType(columnName), 0, 0);
-                  switch (type.getMinorType()) {
-                    case INT:
-                    case TIME:
-                      simpleGroup.append(name + "_minValue", Integer.parseInt(String.valueOf(column.getMinValue())));
-                      simpleGroup.append(name + "_maxValue", Integer.parseInt(String.valueOf(column.getMaxValue())));
-                      break;
-                    case BIGINT:
-                    case TIMESTAMP:
-                      simpleGroup.append(name + "_minValue", Long.parseLong(String.valueOf(column.getMinValue())));
-                      simpleGroup.append(name + "_maxValue", Long.parseLong(String.valueOf(column.getMaxValue())));
-                      break;
-                    case FLOAT4:
-                      simpleGroup.append(name + "_minValue", Float.parseFloat(String.valueOf(column.getMinValue())));
-                      simpleGroup.append(name + "_maxValue", Float.parseFloat(String.valueOf(column.getMaxValue())));
-                      break;
-                    case FLOAT8:
-                      simpleGroup.append(name + "_minValue", Double.parseDouble(String.valueOf(column.getMinValue())));
-                      simpleGroup.append(name + "_maxValue", Double.parseDouble(String.valueOf(column.getMaxValue())));
-                      break;
-                    case BIT:
-                      simpleGroup.append(name + "_minValue", Boolean.parseBoolean(String.valueOf(column.getMinValue())));
-                      simpleGroup.append(name + "_maxValue", Boolean.parseBoolean(String.valueOf(column.getMaxValue())));
-                      break;
-                    default:
-                      simpleGroup.append(name + "_minValue", String.valueOf(column.getMinValue()));
-                      simpleGroup.append(name + "_maxValue", String.valueOf(column.getMaxValue()));
-                  } */
-                  simpleGroup.append(name + "_nulls", column.getNulls());
-
-                }
+//                for (MetadataBase.ColumnMetadata column : rowGroup.getColumns()) {
+//                  String[] columnName = column.getName();
+//                  String name = String.join(":", columnName);
+//                  simpleGroup.append(name + "_name", name);
+////                  simpleGroup.append(name + "_columnStats", gson.toJson(column));
+//                  simpleGroup.append(name + "_minValue", String.valueOf(column.getMinValue()));
+//                  simpleGroup.append(name + "_maxValue", String.valueOf(column.getMaxValue()));
+//               /*   TypeProtos.MajorType type = ParquetReaderUtility.getType(parquetTableMetadata.getPrimitiveType(columnName), parquetTableMetadata.getOriginalType(columnName), 0, 0);
+//                  switch (type.getMinorType()) {
+//                    case INT:
+//                    case TIME:
+//                      simpleGroup.append(name + "_minValue", Integer.parseInt(String.valueOf(column.getMinValue())));
+//                      simpleGroup.append(name + "_maxValue", Integer.parseInt(String.valueOf(column.getMaxValue())));
+//                      break;
+//                    case BIGINT:
+//                    case TIMESTAMP:
+//                      simpleGroup.append(name + "_minValue", Long.parseLong(String.valueOf(column.getMinValue())));
+//                      simpleGroup.append(name + "_maxValue", Long.parseLong(String.valueOf(column.getMaxValue())));
+//                      break;
+//                    case FLOAT4:
+//                      simpleGroup.append(name + "_minValue", Float.parseFloat(String.valueOf(column.getMinValue())));
+//                      simpleGroup.append(name + "_maxValue", Float.parseFloat(String.valueOf(column.getMaxValue())));
+//                      break;
+//                    case FLOAT8:
+//                      simpleGroup.append(name + "_minValue", Double.parseDouble(String.valueOf(column.getMinValue())));
+//                      simpleGroup.append(name + "_maxValue", Double.parseDouble(String.valueOf(column.getMaxValue())));
+//                      break;
+//                    case BIT:
+//                      simpleGroup.append(name + "_minValue", Boolean.parseBoolean(String.valueOf(column.getMinValue())));
+//                      simpleGroup.append(name + "_maxValue", Boolean.parseBoolean(String.valueOf(column.getMaxValue())));
+//                      break;
+//                    default:
+//                      simpleGroup.append(name + "_minValue", String.valueOf(column.getMinValue()));
+//                      simpleGroup.append(name + "_maxValue", String.valueOf(column.getMaxValue()));
+//                  } */
+//                  simpleGroup.append(name + "_nulls", column.getNulls());
+//
+//                }
         metadataWriter.write(simpleGroup);
       }
     }
